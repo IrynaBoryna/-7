8a40c150119391c6b508e79138c4d8ac200fe67d
IrynaBoryna <borisov_ka@ukr.net>
2023-02-26 12:22:42 +0200

    Update 02-lightbox.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -48,3 +48,10 @@ const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', caption
 // const activGalleryItem = event.target;
 
 // activGalleryItem.classList.add('.is-active');
+// const currentActiveItem = document.querySelector('a.is-active');
+// if(currentActiveItem) {
+//   currentActiveItem.classList.remove('.is-active');
+// }
+// const activGalleryItem = event.target;
+
+// activGalleryItem.classList.add('.is-active');
